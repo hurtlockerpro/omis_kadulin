9e6bd225c71c980e4f28fb7ba12a02811898dde0
Vladimir Kjahrenov <info@hurtlocker.pro>
2021-05-24 17:53:03 +0300

    calc v1

---
+++ calc/js/main.js
@@ -0,0 +1,20 @@
+
+// ES6 - 2016
+
+class Calc {
+    
+
+    getX(x){
+        // TODO
+        // 
+        return x
+    }
+
+}
+
+let calc = new Calc()
+calc.getX(15)
+
+
+let calc2 = new Calc()
+calc2.getX(50)
